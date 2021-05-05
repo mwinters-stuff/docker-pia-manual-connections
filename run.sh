d90930136664d72906c2d8fca0cfc575646b8139
Mathew Winters <mathew@winters.org.nz>
2021-05-05 16:46:33 +1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,5 +15,17 @@ now_sleep () {
 
 ./run_setup.sh
 
+if [ -n "$LOCAL_NETWORK" ]; then
+  for range in ${LOCAL_NETWORK//,/ }; do
+    if [ $FIREWALL -eq 1 ]; then
+      echo "$(date): Allowing network access to $range"
+      iptables -A OUTPUT -o eth0 --destination $range -j ACCEPT
+      iptables -A INPUT -i eth0 --source $range -j ACCEPT
+    fi
+    echo "$(date): Adding route to $range"
+    ip route add $range via $(ip route show 0.0.0.0/0 dev eth0 | cut -d\  -f3)
+  done
+fi
+
 now_sleep
 
